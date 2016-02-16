10b8617a272252c3191cbade00f7d90c6d6bda61
Alex Gray <alex@mrgray.com>
2016-02-16 02:11:46 -0500

    Initial Commit

---
+++ WeakLinks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WeakLinks
+//
+//  Created by Alex Gray on 2/16/16.
+//  Copyright © 2016 MrGray.com, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
